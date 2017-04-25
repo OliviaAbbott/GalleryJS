d8dffb6d266d03ec5fb2417fe348655bea5d1187
Unknown <oabbott@mail.csuchico.edu>
2017-04-25 12:24:31 -0700

    4/25/17

--- gallery.js
+++ gallery.js
@@ -1,6 +1,12 @@
-
+var chico = ["One Mile", "Monkey Face", "Sierra Nevada Brewery", "Chico State", "Tin Roof Bakery"];
+var current = "";
 
 function shuffleGallery()
 {
-
+  do
+  {
+    var i = Math.floor(Math.random() * chico.length);
+  }while(current == chico[i])
+  current = chico[i];
+  console.log(chico[i]);
 }
